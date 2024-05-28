19048e29bf442c247dd83163592f0452579f29d3
jairosanchezb94 <jairosanchezb5@gmail.com>
2024-05-28 14:06:24 +0200

    connect data with github improvement

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,82 +1,91 @@
-/* eslint-disable react/jsx-no-target-blank */
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './portfolio.css'
-import p1 from '../../assets/p1.jpg'
-import p2 from '../../assets/p2.jpg'
-import p3 from '../../assets/p3.jpg'
-import p4 from '../../assets/p4.jpg'
-import p5 from '../../assets/p5.jpg'
-import p6 from '../../assets/p6.jpg'
-
-
-const data = [
-  {
-    id: '1',
-    image: p1,
-    title: 'Peliculas',
-    github: 'https://github.com/jairosanchezb94/PeliculasjsApp',
-    demo: 'https://jairosanchezb94.github.io/PeliculasjsApp/home'
-  },
-  {
-    id: '2',
-    image: p2,
-    title: 'Todo List',
-    github: 'https://github.com/jairosanchezb94/todo-list',
-  },
-  {
-    id: '3',
-    image: p3,
-    title: 'Web Tiempo',
-    github: 'https://github.com/jairosanchezb94/web-tiempo',
-    demo: 'https://jairosanchezb94.github.io/web-tiempo/'
-  },
-  {
-    id: '4',
-    image: p4,
-    title: 'Calculadora',
-    github: 'https://github.com/jairosanchezb94/calculadora',
-    demo: 'https://jairosanchezb94.github.io/calculadora/'
-  },
-  {
-    id: '5',
-    image: p5,
-    title: 'Rick and Morty',
-    github: 'https://github.com/jairosanchezb94/rick-and-morty',
-    demo: 'https://jairosanchezb94.github.io/rick-and-morty/usuarios'
-  },
-  {
-    id: '6',
-    image: p6,
-    title: 'Notas',
-    github: 'https://github.com/jairosanchezb94/notas-app',
-    demo: 'https://jairosanchezb94.github.io/notas-app/home'
-  },
-]
 
 const Portfolio = () => {
-  return (
-    <section id="portfolio">
-      <h2>Proyectos</h2>
-      <div className="container portafolio__container">
-        {
-          data.map(({id, image, title, github, demo}) => {
-            return (
-              <article key={id} className="portafolio__item">
-                <div className="portafolio__item-image">
-                  <img src={image} alt={title} />
-                </div>
-                <h3>{title}</h3>
-                <div className="portafolio__item-cta">
-                  <a href={github} className='btn' target='_blank'>Github</a>
-                  <a href={demo} className='btn btn-primary' target='_blank'>Demo</a>
-                </div>
-            </article>
-            )
-          })
+    const [projects, setProjects] = useState([]);
+    const token = process.env.REACT_APP_GITHUB_TOKEN;
+    const username = process.env.REACT_APP_GITHUB_USERNAME;
+
+    useEffect(() => {
+      const fetchRepos = async () => {
+          try {
+              const response = await fetch(`https://api.github.com/users/${username}/repos`, {
+                  headers: {
+                      'Authorization': `token ${token}`
+                  }
+              });
+              if (!response.ok) {
+                  throw new Error('Network response was not ok');
+              }
+              const data = await response.json();
+              const projects = await Promise.all(data.map(async (repo) => {
+                let readmeImage = null;
+                try {
+                    const readmeResponse = await fetch(`https://api.github.com/repos/${username}/${repo.name}/readme`, {
+                        headers: {
+                            'Authorization': `token ${token}`,
+                            'Accept': 'application/vnd.github.v3.raw'
+                        }
+                    });
+                    if (readmeResponse.ok) {
+                        const readmeText = await readmeResponse.text();
+                        const imageMatch = readmeText.match(/!\[.*?\]\((https:\/\/user-images\.githubusercontent\.com\/.*?)\)/);
+                        if (imageMatch) {
+                            readmeImage = imageMatch[1];
+                        }
+                    }
+                } catch (readmeError) {
+                    console.error('Error fetching README:', readmeError);
+                }
+                return {
+                    name: repo.name,
+                    html_url: repo.html_url,
+                    description: repo.description,
+                    image: readmeImage,
+                    topics: repo.topics || [],
+                    homepage: repo.homepage || null
+                };
+            }));
+            setProjects(projects);
+        } catch (error) {
+            console.error('Error fetching projects:', error);
         }
-      </div>   
-    </section>
-  )
-}
+    };
+
+    fetchRepos();
+}, [username, token]);
+
+    return (
+      <section id="portfolio">
+            <h2>Proyectos</h2>
+            <div className="container portafolio__container">
+                {
+                    projects.map((project, index) => (
+                        <article key={index} className="portafolio__item">
+                            {project.image && (
+                                <div className="portafolio__item-image">
+                                    <img src={project.image} alt={project.name} className="readme-image" />
+                                </div>
+                            )}
+                            <h3>{project.name}</h3>
+                            <p>{project.description}</p>
+                            <div className="portfolio__item-topics">
+                                {project.topics.map((topic, i) => (
+                                    <span key={i} className="topic">{topic}</span>
+                                ))}
+                            </div>
+                            <div className="portafolio__item-cta">
+                                <a href={project.html_url} className='btn btn-primary' target='_blank' rel='noopener noreferrer'>Github</a>
+                                {project.homepage && (
+                                    <a href={project.homepage} className='btn btn-primary' target='_blank' rel='noopener noreferrer'>Homepage</a>
+                                )}
+                            </div>
+                        </article>
+                    ))
+                }
+            </div>
+        </section>
+    );
+};
 
 export default Portfolio;
