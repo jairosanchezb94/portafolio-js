99300489316e40bec53ad0e89d4e8a26c6b29efa
jairosanchezb94 <jairosanchezb5@gmail.com>
2024-05-28 15:23:17 +0200

    connect data 2

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,8 +3,8 @@ import './portfolio.css'
 
 const Portfolio = () => {
     const [projects, setProjects] = useState([]);
-    const token = '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';
-    const username = 'jairosanchezb94';
+    const token = process.env.REACT_APP_GITHUB_TOKEN;
+    const username = process.env.REACT_APP_GITHUB_USERNAME;
 
     useEffect(() => {
       const fetchRepos = async () => {
